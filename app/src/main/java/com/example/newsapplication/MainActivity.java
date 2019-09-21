8e3f2d0c1f34e9854317f5807d1b14f3c5bef8b8
Ashwini Raut <ashwiniraut124@gmail.com>
2019-09-21 11:35:55 -0700

    hi

--- app/src/main/java/com/example/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -6,6 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerview;
     RecyclerView.LayoutManager mLayoutManager;
@@ -25,6 +32,32 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerview=findViewById(R.id.mRecyclerView);
         mLayoutManager=new LinearLayoutManager(MainActivity.this);
         mRecyclerview.setLayoutManager(mLayoutManager);
+
+        callAPI();
+    }
+
+    private void callAPI() {
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                       // textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
